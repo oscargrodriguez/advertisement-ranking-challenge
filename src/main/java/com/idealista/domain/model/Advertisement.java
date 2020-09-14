3e1628c6a510c4fe26116c825db27a7712ce23dd
Oscar Gueita <oscar.gueita@lastminute.com>
2020-09-14 17:01:31 +0200

    Ad with only descriptiveText case

--- src/main/java/com/idealista/domain/model/Advertisement.java
+++ src/main/java/com/idealista/domain/model/Advertisement.java
@@ -1,7 +1,21 @@
 package com.idealista.domain.model;
 
 public class Advertisement {
+
+    private String descriptiveText = "";
+
+    public Advertisement() {
+    }
+
+    public Advertisement(String descriptiveText) {
+        this.descriptiveText = descriptiveText;
+    }
+
     public int score() {
-        return 0;
+        if (descriptiveText.isEmpty())
+        {
+            return 0;
+        }
+        return 5;
     }
 }
--- src/main/test/com/idealista/domain/model/AdvertisementTest.java
+++ src/main/test/com/idealista/domain/model/AdvertisementTest.java
@@ -11,4 +11,10 @@ public class AdvertisementTest {
         Advertisement advertisement = new Advertisement();
         assertEquals(0, advertisement.score());
     }
+
+    @Test
+    void descriptiveText() {
+        Advertisement advertisement = new Advertisement("Any text");
+        assertEquals(5, advertisement.score());
+    }
 }
