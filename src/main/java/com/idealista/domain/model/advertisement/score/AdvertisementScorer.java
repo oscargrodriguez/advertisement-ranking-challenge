1f146cc0ceaeedaaa5866e58205ffb7ca2c24da6
Oscar Gueita <oscar.gueita@lastminute.com>
2020-09-17 19:46:08 +0200

    Minor changes

--- src/main/java/com/idealista/domain/model/advertisement/score/AdvertisementScorer.java
+++ src/main/java/com/idealista/domain/model/advertisement/score/AdvertisementScorer.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Function;
+
 @Component
 public class AdvertisementScorer {
     @Autowired
@@ -14,17 +16,16 @@ public class AdvertisementScorer {
     private DescriptionScorer descriptionScorer;
     @Autowired
     private FullAdvertisementScorer fullAdvertisementScorer;
-
     @Value("${score.max}")
     private int maxValue;
     @Value("${score.min}")
     private int minValue;
 
     public int score(Advertisement advertisement) {
-        return checkLimits(getScore(advertisement));
+        return checkLimits(calculateScore(advertisement));
     }
 
-    private int getScore(Advertisement advertisement) {
+    private int calculateScore(Advertisement advertisement) {
         return photoScorer.score(advertisement.getPhotoList()) +
                 descriptionScorer.score(advertisement.getTypology(), advertisement.getDescription()) +
                 fullAdvertisementScorer.score(advertisement);
