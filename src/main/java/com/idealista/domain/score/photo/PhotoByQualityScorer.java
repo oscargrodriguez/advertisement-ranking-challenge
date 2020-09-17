723a3219129fe941d4365bbaa5c8c6a893808f3b
Oscar Gueita <oscar.gueita@lastminute.com>
2020-09-18 00:58:43 +0200

    Adding more properties

--- src/main/java/com/idealista/domain/score/photo/PhotoByQualityScorer.java
+++ src/main/java/com/idealista/domain/score/photo/PhotoByQualityScorer.java
@@ -1,15 +1,18 @@
 package com.idealista.domain.score.photo;
 
 import com.idealista.domain.model.advertisement.photo.Photo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PhotoByQualityScorer {
 
-    private static final int HD_SCORE = 20;
-    private static final int STANDARD_SCORE = 10;
+    @Value("${score.photo.quality.high_definition}")
+    private int hdScore;
+    @Value("${score.photo.quality.standard}")
+    private int standardScore;
 
     public int score(Photo photo) {
-        return photo.isHighDefinition() ? HD_SCORE : STANDARD_SCORE;
+        return photo.isHighDefinition() ? hdScore : standardScore;
     }
 }
