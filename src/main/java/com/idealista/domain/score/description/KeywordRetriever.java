a3dd4965b2624f311d7c6c3d89b0212b4726307b
Oscar G <oscarg@Oscars-MacBook-Pro.local>
2020-09-24 16:31:29 +0200

    Minor refactor KeywordRetriever

--- src/main/java/com/idealista/domain/score/description/KeywordRetriever.java
+++ src/main/java/com/idealista/domain/score/description/KeywordRetriever.java
@@ -3,25 +3,23 @@ package com.idealista.domain.score.description;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class KeywordRetriever {
 
+    private List<String> KEYWORDS = Arrays.asList("luminoso", "nuevo", "centrico", "céntrico", "reformado", "ático", "atico");
+
     public int getNumberOfKeywords(String text) {
         return Math.toIntExact(Arrays.stream(text.split(" "))
                 .map(it -> it.toLowerCase())
-                .filter(it -> isKeyword(it))
+                .filter(it -> isKeyword().test(it))
                 .distinct()
                 .count());
     }
 
-    private boolean isKeyword(String word) {
-        return word.equals("luminoso") ||
-                word.equals("nuevo") ||
-                word.equals("centrico") ||
-                word.equals("céntrico") ||
-                word.equals("reformado") ||
-                word.equals("ático") ||
-                word.equals("atico");
+    private Predicate<String> isKeyword() {
+        return wo -> KEYWORDS.contains(wo);
     }
 }
